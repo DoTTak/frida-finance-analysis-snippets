24e0f116c248b08f283335fb9448959d2b4310e3
DoTTak <kdw8220@gmail.com>
2024-03-08 23:15:29 +0900

    add random_android_id.js

---
+++ random_android_id.js
@@ -0,0 +1,17 @@
+Java.performNow(function () {
+    var rand_1 = Math.random().toString(16).substring(2, 10)
+    var rand_2 = Math.random().toString(16).substring(2, 10)
+    const RANDOM_ANDROID_ID = rand_1 + rand_2
+
+    Java.use("android.provider.Settings$Secure").getString.overload("android.content.ContentResolver", "java.lang.String").implementation = function(context, param) {
+        if (param == Java.use("android.provider.Settings$Secure").ANDROID_ID.value) {
+            return RANDOM_ANDROID_ID;
+        } else {
+            return this.getString(context, param);
+        }
+    }
+
+    Java.use("android.provider.Settings$Secure").getStringForUser.implementation = function (a, b, c) {
+        return RANDOM_ANDROID_ID
+    }
+})
