b48b832755983efbcf6dc16784ea33a3b8336baf
DoTTak <kdw8220@gmail.com>
2024-03-09 00:56:48 +0900

    add random_android_id.js

---
+++ maps_get_library.js
@@ -0,0 +1,25 @@
+Java.perform(function () {
+    const aFile = Java.use('java.io.File');
+    const FileReader = Java.use('java.io.FileReader');
+    const BufferedReader = Java.use('java.io.BufferedReader');
+
+    var sourceFile = aFile.$new.overload('java.lang.String').call(aFile, '/proc/self/maps');
+    var fileReader = FileReader.$new.call(FileReader, sourceFile)
+    var reader = BufferedReader.$new.call(BufferedReader, fileReader)
+
+    var data = null;
+    while ((data = reader.readLine()) != null) {
+        if (data.indexOf("<PACKAGE_NAME>") > -1 && data.indexOf("<LIB_NAME>") > -1) {
+            is_next_catch = true
+            console.warn(data)
+            var addr = data.split(" ")[0].split("-")
+            var start = parseInt("0x" + addr[0], 16)
+            var end = parseInt("0x" + addr[1], 16)
+
+            var file = new File("/data/data/<PACKAGE_NAME>/dump_<LIB_NAME>", "ab")
+            file.write(Memory.readByteArray(ptr(start), end - start))
+            file.flush()
+            file.close()
+        }
+    }
+})
