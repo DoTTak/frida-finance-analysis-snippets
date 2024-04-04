e16206bbe9762f8617645676d182d358236bdb22
DoTTak <kdw8220@gmail.com>
2024-04-04 13:38:37 +0900

    Update string_related.js

--- string_related.js
+++ string_related.js
@@ -5,3 +5,23 @@ Java.performNow(function(){
         return retval;
     }
 });
+
+Interceptor.attach(Module.findExportByName(null, "strstr"), {
+    onEnter: function (args) {
+        var haystack = Memory.readUtf8String(args[0])
+        var needle = Memory.readUtf8String(args[1])
+        console.log("[*] strstr("+haystack+", "+needle+")")
+    },
+    onLeave: function (retval) {}
+});
+
+Interceptor.attach(Module.findExportByName(null, "memmem"), {
+    onEnter: function (args) {
+        var haystack = Memory.readUtf8String(args[0])
+        var haystacklen = args[1]
+        var needle = Memory.readUtf8String(args[2])
+        var needlelen = args[3]
+        console.log("[*] memmem("+haystack+", "+haystacklen+", "+needle+", "+needlelen+")")
+    },
+    onLeave: function (retval) {}
+});
