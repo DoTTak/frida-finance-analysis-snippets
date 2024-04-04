15abc9cbfa61d15e4f308a0cbbb391e41e88a5b5
DoTTak <kdw8220@gmail.com>
2024-04-04 12:45:13 +0900

    Update file_related.js

--- file_related.js
+++ file_related.js
@@ -13,3 +13,28 @@ Interceptor.attach(Module.findExportByName(null, "access"), {
     },
     onLeave: function (retval) {}
 });
+
+Interceptor.attach(Module.findExportByName(null, "fopen"), {
+    onEnter: function (args) {
+        var path = Memory.readUtf8String(args[0])
+        var perm = Memory.readUtf8String(args[1])
+        console.log("[*] fopen("+path+", "+perm+")")
+    },
+    onLeave: function (retval) {}
+});
+
+Interceptor.attach(Module.findExportByName(null, "stat"), {
+    onEnter: function (args) {
+        var path = Memory.readUtf8String(args[0])
+        console.log("[*] stat("+path+")")
+    },
+    onLeave: function (retval) {}
+});
+
+Interceptor.attach(Module.findExportByName(null, "lstat"), {
+    onEnter: function (args) {
+        var path = Memory.readUtf8String(args[0])
+        console.log("[*] lstat("+path+")")
+    },
+    onLeave: function (retval) {}
+});
