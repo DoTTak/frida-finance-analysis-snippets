224f533d98d47dac4633936009aedc6ffd6e6e05
DoTTak <kdw8220@gmail.com>
2024-04-05 10:18:18 +0900

    Update BYPASS_VARIABLE_LIST.js

--- BYPASS_VARIABLE_LIST.js
+++ BYPASS_VARIABLE_LIST.js
@@ -1,8 +1,12 @@
 const ROOTING_BINARIES = [
-    "/sbin/.magisk", "/sbin/magiskhide", "/sbin/magiskinit", "/sbin/magiskpolicy",
-    "/sbin/su", "/sbin/supolicy"
+    "magisk", "/sbin/.magisk", "/sbin/magisk", "/sbin/magiskhide", "/sbin/magiskinit", "/sbin/magiskpolicy",
+    "/sbin/su", "/sbin/supolicy", 
 ];
 
 const ROOTING_PACKAGES = [
     "com.topjohnwu.magisk"
 ];
+
+const FRIDA_BINARIES = [
+    "frida", "frida-agent", "re.frida.server", "linjector"
+]
