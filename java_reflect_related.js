c707c7fe85725dc2c9ad83dc08b6460e6bdc6c05
DoTTak <kdw8220@gmail.com>
2024-05-07 12:56:58 +0900

    Update: java_reflect_related.js

--- java_reflect_related.js
+++ java_reflect_related.js
@@ -1,9 +1,9 @@
 Java.use("java.lang.Class").getDeclaredField.implementation = function(name){
-    console.log("[getDeclaredField] " + this.toString() + "." + name)    
+    console.log("[*] Class.getDeclaredField("+ this.toString() + "." + name +")")    
     return this.getDeclaredField(name)
 }
 
 Java.use("java.lang.Class").forName.overload('java.lang.String').implementation = function(className){
-    console.log("[forName] " + className)    
+    console.log("[*] Class.forName("+ className +")")    
     return this.forName(className)
 }
