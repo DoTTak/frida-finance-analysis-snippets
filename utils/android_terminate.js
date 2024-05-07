273c380d0a754b7643fade1866f07f4fe98ad3d6
DoTTak <kdw8220@gmail.com>
2024-05-07 13:07:57 +0900

    Create: utils/android_terminate.js

---
+++ utils/android_terminate.js
@@ -0,0 +1,25 @@
+Java.use("java.lang.System").exit.implementation = function () {
+    console.log("[*] System.exit()")
+    this.exit()
+}
+
+Java.use("android.app.Activity").finish.overload().implementation = function () {
+    console.log("[*] Activity.finish()")
+    this.finish()
+}
+
+Java.use("android.os.Process").killProcess.implementation = function (pid) {
+    console.log("[*] Process.killProcess(" + pid + ")")
+    this.killProcess()
+}
+
+Interceptor.attach(Module.findExportByName(null, "exit"), function(args){
+    var status = Memory.readInt(args[0])
+    console.log("[*] exit("+status+")")
+})
+
+Interceptor.attach(Module.findExportByName(null, "kill"), function(args){
+    var pid = parseInt(args[0], 16)
+    var sig = args[1]
+    console.log("[*] kill("+pid+", "+ sig +")")
+})
