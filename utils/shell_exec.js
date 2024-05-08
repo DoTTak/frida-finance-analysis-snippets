227fc55386b6d5d3d3b3cb2001ef5d9c6fbea091
DoTTak <kdw8220@gmail.com>
2024-05-08 14:46:33 +0900

    Update: utils/shell_exec.js

--- utils/shell_exec.js
+++ utils/shell_exec.js
@@ -1,6 +1,12 @@
 /**
+ * NOTICE:
+ * - You must call the function using the Java.perform() function.
+ * 
  * Example) 
- * shell_exec("id")
+ * Java.perform(function(){
+ *     shell_exec("id")
+ * })
+ * 
  * frida $ id
  * uid=1000(system) gid=1000(system) groups=1000(system),1002(bluetooth),1007(log),1023(media_rw),1024(mtp),1065(reserved_disk),2001(cache),3001(net_bt_admin),3002(net_bt),3003(inet),3007(net_bw_acct),3010(wakelock),3011(uhid),5050(vendor_secdir),9997(everybody),9997(everybody) context=u:r:system_app:s0
  */
